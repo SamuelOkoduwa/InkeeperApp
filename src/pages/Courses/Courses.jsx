0408eeef9533a9a1a6ccdb489e99a7c89e80402a
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-06-14 16:35:15 +0100

    Strucured the pages

---
+++ src/pages/Courses/Courses.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Courses() {
+  return (
+	<div>
+	  Welcome to Courses page
+	</div>
+  )
+}
+
+export default Courses
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+	<div>Welcome to InKeeper</div>
+  )
+}
+
+export default Home
