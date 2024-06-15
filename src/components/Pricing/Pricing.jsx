870583e0ce6b1e79f252b392e8f45d4447b644e8
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-06-15 22:13:02 +0100

    Added a header style

--- src/components/Pricing/Pricing.jsx
+++ src/components/Pricing/Pricing.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import styles from './Pricing.module.css'
 
 function Pricing() {
   return (
-	<div>Pricing</div>
+	<div className={styles.price}>
+    <h3>Amazing Payment plans for you</h3>
+    <p>Our payment options have amazing impact on how you learn</p>
+  </div>
   )
 }
 
