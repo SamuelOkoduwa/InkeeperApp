8924afe1c4e2facf58f617e673f2c5cbacf8f787
Toluwani309 <toluwanimi309@gmail.com>
2024-06-17 21:40:55 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
-import React from "react"
-import NavBar from "./components/NavBar/NavBar"
-import Home from "./pages/Home/Home"
-import About from "./pages/About/About"
-import Courses from "./pages/Courses/Courses"
-import Contact from "./pages/Contact/Contact"
-import ApplyNow from "./pages/ApplyNow/ApplyNow"
-import { Route, Routes, BrowserRouter} from "react-router-dom"
+import React from "react";
+import NavBar from "./components/NavBar/NavBar";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Courses from "./pages/Courses/Courses";
+import Contact from "./pages/Contact/Contact";
+import ApplyNow from "./pages/ApplyNow/ApplyNow";
+import AmazingCompany from "./components/AmazingCompaniesIntern/AmazingCompany";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div>
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/courses" element={<Courses />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/applyNow" element={<ApplyNow />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/applyNow" element={<ApplyNow />} />
+        </Routes>
+        <AmazingCompany />
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/AmazingCompaniesIntern/AmazingCompany.jsx
+++ src/components/AmazingCompaniesIntern/AmazingCompany.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import "./AmazingCompany.module.css";
+import gear from "../../assets/Images/gear.png";
+import digiplus from "../../assets/Images/digiplus.png";
+import eco from "../../assets/Images/eco.png";
+import haven from "../../assets/Images/haven.png";
+import Trulance from "../../assets/Images/Trulance.png";
+import union from "../../assets/Images/union.png";
 
 function AmazingCompany() {
   return (
-	<div>AmazingCompany</div>
-  )
+    <>
+      <div AmazingCompany>
+        <div class="container">
+          <div class="write-up">
+            <h5>Amazing companies our interns work with</h5>
+            <p>
+              These companies have worked with our interns and have given us
+              amazing feedback.
+            </p>
+          </div>
+          <div class="image-container">
+            <img src={gear.png} />
+            <img src={digiplus.png} />
+            <img src={eco.png} />
+            <img src={haven.png} />
+            <img src={Trulance.png} />
+            <img src={union.png} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default AmazingCompany
+export default AmazingCompany;
