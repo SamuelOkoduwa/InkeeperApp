55fd68e5ce20d7dabf4859ce0ddf888ae13b7ef2
Toluwani309 <toluwanimi309@gmail.com>
2024-06-18 00:42:19 +0100

    commit

--- src/components/AmazingCompaniesIntern/AmazingCompany.jsx
+++ src/components/AmazingCompaniesIntern/AmazingCompany.jsx
@@ -4,28 +4,30 @@ import gear from "../../assets/Images/gear.png";
 import digiplus from "../../assets/Images/digiplus.png";
 import eco from "../../assets/Images/eco.png";
 import haven from "../../assets/Images/haven.png";
-import Trulance from "../../assets/Images/Trulance.png";
-import union from "../../assets/Images/union.png";
+import Legal from "../../assets/Images/Legal.png";
+import union from "../../assets/Images/Trulance.png";
+import Trulance from "../../assets/Images/union.png";
 
 function AmazingCompany() {
   return (
     <>
       <div AmazingCompany>
-        <div class="container">
-          <div class="write-up">
+        <div className="container">
+          <div className="write-up">
             <h5>Amazing companies our interns work with</h5>
             <p>
               These companies have worked with our interns and have given us
               amazing feedback.
             </p>
           </div>
-          <div class="image-container">
-            <img src={gear.png} />
-            <img src={digiplus.png} />
-            <img src={eco.png} />
-            <img src={haven.png} />
-            <img src={Trulance.png} />
-            <img src={union.png} />
+          <div className="image-container">
+            <img src={gear} />
+            <img src={digiplus} />
+            <img src={eco} />
+            <img src={haven} />
+            <img src={Legal} />
+            <img src={union} />
+            <img src={Trulance} />
           </div>
         </div>
       </div>
