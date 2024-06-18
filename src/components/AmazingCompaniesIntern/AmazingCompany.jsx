db8e6a3eab3fbe56bdea78d5b6668c2a6af14385
Toluwani309 <toluwanimi309@gmail.com>
2024-06-18 20:11:58 +0100

    commit

--- src/components/AmazingCompaniesIntern/AmazingCompany.jsx
+++ src/components/AmazingCompaniesIntern/AmazingCompany.jsx
@@ -1,37 +1,35 @@
 import React from "react";
-import "./AmazingCompany.module.css";
-import gear from "../../assets/Images/gear.png";
-import digiplus from "../../assets/Images/digiplus.png";
-import eco from "../../assets/Images/eco.png";
-import haven from "../../assets/Images/haven.png";
-import Legal from "../../assets/Images/Legal.png";
-import union from "../../assets/Images/Trulance.png";
-import Trulance from "../../assets/Images/union.png";
+import styles from "./AmazingCompanies.module.css";
+import thgear from "../../assets/Images/thgear.png";
+import digi from "../../assets/Images/digi.png";
+import barter from "../../assets/Images/barter.png";
+import edu from "../../assets/Images/edu.png";
+import LM from "../../assets/Images/LM.png";
+import Bank from "../../assets/Images/Bank.png";
+import lance from "../../assets/Images/lance.png";
 
 function AmazingCompany() {
   return (
-    <>
-      <div AmazingCompany>
-        <div className="container">
-          <div className="write-up">
-            <h5>Amazing companies our interns work with</h5>
-            <p>
-              These companies have worked with our interns and have given us
-              amazing feedback.
-            </p>
-          </div>
-          <div className="image-container">
-            <img src={gear} />
-            <img src={digiplus} />
-            <img src={eco} />
-            <img src={haven} />
-            <img src={Legal} />
-            <img src={union} />
-            <img src={Trulance} />
-          </div>
-        </div>
+    <div className={styles.container}>
+      <div className={styles.writeup}>
+        <h5 className={styles.heading}>
+          Amazing companies our interns work with
+        </h5>
+        <p className={styles.bodytext}>
+          These companies have worked with our interns and have given us amazing
+          feedback.
+        </p>
       </div>
-    </>
+      <div className={styles.imagecontainer}>
+        <img src={thgear} />
+        <img src={digi} />
+        <img src={barter} />
+        <img src={edu} />
+        <img src={LM} />
+        <img src={Bank} />
+        <img src={lance} />
+      </div>
+    </div>
   );
 }
 
