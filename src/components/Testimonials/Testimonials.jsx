716524a8ff176f00126c2868074f10b9abd61421
Tobi Isaac <tobiisaac428@gmail.com>
2024-06-18 16:39:34 +0100

    bootstrap

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Carousel from "react-bootstrap/Carousel";
+import { useMediaQuery } from "react-responsive";
 import styles from "./Testimonials.module.css";
 import Zoe from "../../assets/images/UI/Zoe.png";
 import Mantis from "../../assets/images/UI/Mantis.png";
@@ -29,37 +29,58 @@ const testimonialsData = [
     testimonial:
       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod",
   },
+  {
+    name: "Zoe Mantis",
+    role: "Founder, Alpha Group",
+    image: Zoe,
+    testimonial:
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod",
+  },
+  {
+    name: "Zoe Mantis",
+    role: "Founder, Alpha Group",
+    image: Mantis,
+    testimonial:
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod",
+  },
+  {
+    name: "Zoe Mantis",
+    role: "Founder, Alpha Group",
+    image: man,
+    testimonial:
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod",
+  },
 ];
 
 const Testimonials = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3, // Number of testimonials to show in a row
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3500,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
+  // Define breakpoints for different screen sizes
+  const isSmallScreen = useMediaQuery({ query: "(max-width: 576px)" });
+  const isMediumScreen = useMediaQuery({
+    query: "(min-width: 577px) and (max-width: 768px)",
+  });
+  const isLargeScreen = useMediaQuery({ query: "(min-width: 769px)" });
+
+  // Determine the number of items per slide based on screen size
+  const itemsPerSlide = isSmallScreen ? 1 : isMediumScreen ? 2 : 3;
+
+  // Function to chunk the testimonials data into groups of itemsPerSlide
+  const chunkedTestimonialsData = (data, size) => {
+    let results = [];
+    while (data.length) {
+      results.push(data.splice(0, size));
+    }
+    return results;
   };
+
+  // Chunk the testimonials data into groups for the carousel
+  const testimonialChunks = chunkedTestimonialsData(
+    [...testimonialsData],
+    itemsPerSlide
+  );
+
   return (
-    <div className="styles.container" style={{ backgroundColor: "#F5F8F8" }}>
-      <div className="styles.titlecontainer">
+    <div className={styles.wholecontainer}>
+      <div className={styles.titlecontainer}>
         <h4>What our students are saying?</h4>
         <p>
           Read what our students are saying about us and be informed on your
@@ -67,28 +88,34 @@ const Testimonials = () => {
         </p>
       </div>
 
-      <Slider {...settings}>
-        {testimonialsData.map((testimonial, index) => (
-          <div key={index} className={styles.testimonialContainer}>
-            <div className={styles.testimonial}>
-              <div className={styles.testimonialInfo}>
-                <img
-                  className={styles.testimonialImg}
-                  src={testimonial.image}
-                  alt={testimonial.name}
-                />
-                <div className={styles.nameRole}>
-                  <h3>{testimonial.name}</h3>
-                  <p>{testimonial.role}</p>
+      <Carousel interval={3000}>
+        {testimonialChunks.map((chunk, chunkIndex) => (
+          <Carousel.Item key={chunkIndex}>
+            <div
+              className={`d-flex justify-content-around ${styles.testimonialContainer}`}
+            >
+              {chunk.map((testimonial, testimonialIndex) => (
+                <div key={testimonialIndex} className={`${styles.testimonial}`}>
+                  <div className={styles.testimonialInfo}>
+                    <img
+                      className={styles.testimonialImg}
+                      src={testimonial.image}
+                      alt={testimonial.name}
+                    />
+                    <div className={styles.nameRole}>
+                      <h3>{testimonial.name}</h3>
+                      <p>{testimonial.role}</p>
+                    </div>
+                  </div>
+                  <div className={styles.testimonialText}>
+                    <p>{testimonial.testimonial}</p>
+                  </div>
                 </div>
-              </div>
-              <div className={styles.testimonialText}>
-                <p>{testimonial.testimonial}</p>
-              </div>
+              ))}
             </div>
-          </div>
+          </Carousel.Item>
         ))}
-      </Slider>
+      </Carousel>
     </div>
   );
 };
