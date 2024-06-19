f49d7624e8dd8881fa3bddfb9e2d7673042ee823
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-06-19 04:33:05 +0100

    Imported Amazing companies in the About page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import GroupExample from '../../components/Faq/Faq'
 import ContactUs from '../../components/ContactUs/ContactUs';
 import Partners from '../../components/Partners/Partners';
+import AmazingCompany from '../../components/AmazingCompaniesIntern/AmazingCompany';
 
 function About() {
   return (
 	<div>
 		<Partners />
+		<AmazingCompany />
 	  <GroupExample />
 	  <ContactUs />
 	</div>
