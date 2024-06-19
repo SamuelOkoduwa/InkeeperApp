108caa2f9a44401acbb12a1a5528be17753e7ca9
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-06-19 03:32:40 +0100

    Added partners to about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import GroupExample from '../../components/Faq/Faq'
 import ContactUs from '../../components/ContactUs/ContactUs';
+import Partners from '../../components/Partners/Partners';
 
 function About() {
   return (
 	<div>
+		<Partners />
 	  <GroupExample />
 	  <ContactUs />
 	</div>
