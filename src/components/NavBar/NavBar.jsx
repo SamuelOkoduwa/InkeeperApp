b04d6c9f634388e5d1a8646fcd36b39583f446fb
Tobi Isaac <tobiisaac428@gmail.com>
2024-06-20 00:24:18 +0100

    some errors resolved from package.json and Navbar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from './NavBar.module.css';
-import Applogo from "../../assets/Images/Applogo.png"
+import styles from "./NavBar.module.css";
+import Applogo from "../../assets/Images/Applogo.png";
 import arrow_down from "../../assets/Icons/arrow_down.png";
 import Button from "react-bootstrap/Button";
 
@@ -9,24 +9,24 @@ function NavBar() {
   return (
     <nav className={styles.nav}>
       <span>
-        <img src={Applogo} alt="" />
+        <img src={Applogo} alt="App Logo" />
       </span>
       <ul className={styles.ul}>
         <li className="navbarss_menu">
           <Link to="/">Home</Link>
         </li>
 
-        <li navbarss_menu>
+        <li className="navbarss_menu">
           <Link to="/courses">Courses</Link>
-          <img  className="caret" src={arrow_down} alt="" />
+          <img className="caret" src={arrow_down} alt="Dropdown Arrow" />
         </li>
 
-        <li navbarss_menu>
+        <li className="navbarss_menu">
           <Link to="/About">About</Link>
         </li>
-        <li className={"${styles.menu_applyNow}navbarss_menu "}>
+
+        <li className={`navbarss_menu ${styles.menu_applyNow}`}>
           <Link to="/applyNow">Apply Now</Link>
-  
         </li>
       </ul>
     </nav>
