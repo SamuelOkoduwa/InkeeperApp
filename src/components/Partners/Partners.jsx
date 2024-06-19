f7bf26eff0c56f16f8628bb04f59517f74057769
taiwo1212 <taiwotaiwo717@gmail.com>
2024-06-19 10:29:24 +0100

    full-with

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -7,95 +7,98 @@ import Col from "react-bootstrap/Col";
 
 const Partners = () => {
   return (
-    <Container style={{ backgroundColor: "aliceBlue" }}>
-      <br/>
-      <h5>We partner with amazing companies</h5>
-      <p>There are companies that work with us and we love them absolutly.</p>
+    <Container fluid style={{ backgroundColor: "aliceBlue" }}>
+      <br />
 
-      <Row>
-        <Col>
-          <img
-            src="https://res.cloudinary.com/dh3pu2soy/image/upload/v1718580916/Rectangle_16_tzrfto.png"
-            alt=""
-          />
-        </Col>
-        <Col>
-          <img
-            src="https://res.cloudinary.com/dh3pu2soy/image/upload/v1718580918/Rectangle_29pledges_d9jvsw.png"
-            alt=""
-          />
-        </Col>
-        <Col>
-          <img
-            src="https://res.cloudinary.com/dh3pu2soy/image/upload/v1718580916/Rectangle_18dig_plus_ljdoy9.png"
-            alt=""
-          />
-        </Col>
-        <Col>
-          <img
-            src="https://res.cloudinary.com/dh3pu2soy/image/upload/v1718580916/Rectangle_19haven_iicd3w.png"
-            alt=""
-          />
-        </Col>
-        <Col>
-          <img
-            src="https://res.cloudinary.com/dh3pu2soy/image/upload/v1718580916/Rectangle_20legalmo_ydgeyh.png"
-            alt=""
-          />
-        </Col>
-      </Row>
-      <br />
-      <Row>
-        <Col>
-          <img
-            src="https://res.cloudinary.com/dh3pu2soy/image/upload/v1718580917/Rectangle_21microsoft_bkwj2z.png"
-            alt=""
-          />
-        </Col>
-        <Col>
-          <img
-            src="https://res.cloudinary.com/dh3pu2soy/image/upload/v1718580917/Rectangle_22cisco_zbk3jy.png"
-            alt=""
-          />
-        </Col>
-        <Col></Col>
-        <Col>
-          <img
-            src="https://res.cloudinary.com/dh3pu2soy/image/upload/v1718580917/Rectangle_25lsetf_1_fl78lb.png"
-            alt=""
-          />
-        </Col>
-        <Col>
-          <img
-            src="https://res.cloudinary.com/dh3pu2soy/image/upload/v1718580916/Rectangle_26lagos_innovati_wzjjr9.png"
-            alt=""
-          />
-        </Col>
-      </Row>
-      <br />
-      <Row>
-        <Col></Col>
-        <Col>
-          <img
-            src="https://res.cloudinary.com/dh3pu2soy/image/upload/v1718580917/Rectangle_27truelancs_g41o2x.png"
-            alt=""
-          />
-        </Col>
-        <Col>
-          <img
-            src="https://res.cloudinary.com/dh3pu2soy/image/upload/v1718580917/Rectangle_28lagos_state_1_pc1kug.png"
-            alt=""
-          />
-        </Col>
-        <Col>
-          <img
-            src="https://res.cloudinary.com/dh3pu2soy/image/upload/v1718580917/Rectangle_23ift_fjrfdv.png"
-            alt=""
-          />
-        </Col>
-        <Col></Col>
-      </Row>
-      <br/>
+      <Container>
+        <h5>We partner with amazing companies</h5>
+        <p>There are companies that work with us and we love them absolutly.</p>
+
+        <Row>
+          <Col>
+            <img
+              src="https://res.cloudinary.com/dh3pu2soy/image/upload/v1718580916/Rectangle_16_tzrfto.png"
+              alt=""
+            />
+          </Col>
+          <Col>
+            <img
+              src="https://res.cloudinary.com/dh3pu2soy/image/upload/v1718580918/Rectangle_29pledges_d9jvsw.png"
+              alt=""
+            />
+          </Col>
+          <Col>
+            <img
+              src="https://res.cloudinary.com/dh3pu2soy/image/upload/v1718580916/Rectangle_18dig_plus_ljdoy9.png"
+              alt=""
+            />
+          </Col>
+          <Col>
+            <img
+              src="https://res.cloudinary.com/dh3pu2soy/image/upload/v1718580916/Rectangle_19haven_iicd3w.png"
+              alt=""
+            />
+          </Col>
+          <Col>
+            <img
+              src="https://res.cloudinary.com/dh3pu2soy/image/upload/v1718580916/Rectangle_20legalmo_ydgeyh.png"
+              alt=""
+            />
+          </Col>
+        </Row>
+        <br />
+        <Row>
+          <Col>
+            <img
+              src="https://res.cloudinary.com/dh3pu2soy/image/upload/v1718580917/Rectangle_21microsoft_bkwj2z.png"
+              alt=""
+            />
+          </Col>
+          <Col>
+            <img
+              src="https://res.cloudinary.com/dh3pu2soy/image/upload/v1718580917/Rectangle_22cisco_zbk3jy.png"
+              alt=""
+            />
+          </Col>
+          <Col></Col>
+          <Col>
+            <img
+              src="https://res.cloudinary.com/dh3pu2soy/image/upload/v1718580917/Rectangle_25lsetf_1_fl78lb.png"
+              alt=""
+            />
+          </Col>
+          <Col>
+            <img
+              src="https://res.cloudinary.com/dh3pu2soy/image/upload/v1718580916/Rectangle_26lagos_innovati_wzjjr9.png"
+              alt=""
+            />
+          </Col>
+        </Row>
+        <br />
+        <Row>
+          <Col></Col>
+          <Col>
+            <img
+              src="https://res.cloudinary.com/dh3pu2soy/image/upload/v1718580917/Rectangle_27truelancs_g41o2x.png"
+              alt=""
+            />
+          </Col>
+          <Col>
+            <img
+              src="https://res.cloudinary.com/dh3pu2soy/image/upload/v1718580917/Rectangle_28lagos_state_1_pc1kug.png"
+              alt=""
+            />
+          </Col>
+          <Col>
+            <img
+              src="https://res.cloudinary.com/dh3pu2soy/image/upload/v1718580917/Rectangle_23ift_fjrfdv.png"
+              alt=""
+            />
+          </Col>
+          <Col></Col>
+        </Row>
+        <br />
+      </Container>
     </Container>
   );
 };
