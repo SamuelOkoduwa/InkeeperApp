369192389a9333502f5fa449436dbdad555d7ff3
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-06-20 21:30:34 +0100

    updated package.json

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,32 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "src"),
+//     },
+//   },
+//   server: {
+//     port: 3000,
+//     open: true,
+//   },
+//   build: {
+//     outDir: "dist",
+//     minify: "terser",
+//     sourcemap: true,
+//   },
+// });
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-  server: {
-    port: 3000,
-    open: true,
-  },
   build: {
-    outDir: "dist",
-    minify: "terser",
-    sourcemap: true,
-  },
+    sourcemap: false
+  }
 });
