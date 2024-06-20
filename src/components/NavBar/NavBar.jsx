cafea8d3446d6272858c233bccaa9a419ca7e622
Taye <oyeladetaye@gmail.com>
2024-06-20 15:03:46 +0100

    added styles

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,34 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import styles from './NavBar.module.css';
-import Applogo from "../../assets/Images/Applogo.png"
+import styles from "./NavBar.module.css";
+import Applogo from "../../assets/Images/Applogo.png";
 import arrow_down from "../../assets/Icons/arrow_down.png";
-import Button from "react-bootstrap/Button";
+import mennu from "../../assets/Icons/mennu.jpg";
+import close from "../../assets/Icons/close.png";
 
 function NavBar() {
+  const [open, setOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const drps = () => {
+    setOpen(!open);
+  };
+
+  console.log({ menuOpen });
+
   return (
     <nav className={styles.nav}>
-      <span>
-        <img src={Applogo} alt="" />
-      </span>
-      <ul className={styles.ul}>
+      <img src={Applogo} alt="My Logo Image" />
+      <ul className={`${styles.ul} ${menuOpen ? styles.showMenu : ""}`}>
         <li className="navbarss_menu">
           <Link to="/">Home</Link>
         </li>
 
-        <li navbarss_menu>
-          <Link to="/courses">Courses</Link>
-          <img  className="caret" src={arrow_down} alt="" />
+        <li className="navbarss_menu" onClick={drps}>
+          <div className={`${styles.drpdwn}`}>
+            <Link to="/courses">Courses</Link>
+            <span>
+              <img
+                src={arrow_down}
+                alt="Arrow Icon"
+                className={`${styles.arrow}`}
+              />
+            </span>
+            {open && (
+              <div className={`${styles.drpdwn_menu}`}>
+                <p>Front-end Development</p>
+                <p>UI/UX</p>
+                <p>Digital Marketing</p>
+                <p>CyberSecurity</p>
+                <p>Data Science/AI</p>
+                <p>Data Analysis</p>
+              </div>
+            )}
+          </div>
         </li>
 
-        <li navbarss_menu>
-          <Link to="/About">About</Link>
+        <li className="navbarss_menu">
+          <Link to="/about">About</Link>
         </li>
-        <li className={"${styles.menu_applyNow}navbarss_menu "}>
-          <Link to="/applyNow">Apply Now</Link>
-  
+        
+        <li className={`${styles.menu_contacts} navbarss_menu `}>
+          <Link to="/contact">Apply Now</Link>
         </li>
       </ul>
+      {menuOpen ? (
+        <div className={styles.close_icon} onClick={toggleMenu}>
+          <span>
+            <img src={close} alt="Menu Icon" className={styles.close} />
+          </span>
+        </div>
+      ) : (
+        <div className={styles.menu_icon} onClick={toggleMenu}>
+          <span>
+            <img src={mennu} alt="Menu Icon" className={styles.menu} />
+          </span>
+        </div>
+      )}
     </nav>
   );
 }
