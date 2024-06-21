80117ce16a102890448ecd44fde8017bdf88cd0a
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-06-21 10:28:23 +0100

    added Cources the Courses Pages

--- src/pages/Courses/Courses.jsx
+++ src/pages/Courses/Courses.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Pricing from '../../components/Pricing/Pricing'
 import LandingPageHero from '../../components/LandingPageHero/LandingPageHero'
+import OurCourses from '../../components/OurCourses/OurCourses'
 
 function Courses() {
   return (
 	<div className='container'>
 	  <LandingPageHero />
+	  <OurCourses />
 	  <Pricing />
 	</div>
   )
